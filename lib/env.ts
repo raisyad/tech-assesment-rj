a4a0a04cf018a826326494da9557332469492d4b
raisyadjullfikar <raisyadjullfikar@upi.edu>
2026-03-17 13:02:16 +0700

    feat: add env sample

---
+++ lib/env.ts
@@ -0,0 +1,6 @@
+export const env = {
+  LOGIN_API_URL: process.env.LOGIN_API_URL,
+  DEVICE_LOG_API_URL: process.env.DEVICE_LOG_API_URL,
+  SESSION_SECRET: process.env.SESSION_SECRET,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+};
